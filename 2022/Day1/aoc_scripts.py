97c19312b4bd0a94c67a95b0e2114ecd6c958583
Bar0lg <barolgjeu@gmail.com>
2022-12-01 16:50:47 +0100

    Script d'aide cree

---
+++ 2022/Day1/aoc_scripts.py
@@ -0,0 +1,12 @@
+import sys,os,time,math,copy
+import numpy as np
+
+# Liste de str --> Liste de int
+def str_to_int_conv(list_to_conv):
+    res = []
+    for i in list_to_conv:
+        if type(i) == list:
+            res.append(str_to_int_conv(i))
+        else:
+            res.append(int(i))
+    return res
--- 2022/Day1/d1script.py
+++ 2022/Day1/d1script.py
@@ -1,23 +1,18 @@
 import sys,os,time,math,copy
 import numpy as np
 
+import aoc_scripts as aoc
+
 def extract(file):
     f = open(file,"r").read().split("\n\n")
     data = [i.split("\n") for i in f]
     return data
 
-def str_to_int_conv(list_to_conv):
-    res = []
-    for i in list_to_conv:
-        if type(i) == list:
-            res.append(str_to_int_conv(i))
-        else:
-            res.append(int(i))
-    return res
+
         
 
 def main(file):
-    data = str_to_int_conv(extract(file)) 
+    data = AOC.str_to_int_conv(extract(file)) 
 
     #Part 1
     Cal_per_elves = [sum(i) for i in data]
